1a888380dc193e0206e926b364dab6a7b863b40c
Apple <apple@Apples-MacBook-Pro-94.local>
2019-07-31 16:32:58 -0400

    hufih

--- Apptivism/Apptivism/ViewController.swift
+++ Apptivism/Apptivism/ViewController.swift
@@ -12,10 +12,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     var imagePicker = UIImagePickerController()
     
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        imagePicker.delegate = self
 
         
         // Do any additional setup after loading the view.
@@ -29,26 +32,64 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var newImage: UIImageView!
     internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-        {
-            newImage.image = selectedImage }
-        imagePicker.dismiss(animated:true, completion: nil)
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            newImage.image = selectedImage
+            
         }
+        imagePicker.dismiss(animated:true, completion: nil)
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     
     
     
     
-}
+    @IBAction func saveButton(_ sender: UIButton) {
+        let newInfo = nameText.text!
+        displayLabel.text = newInfo
+            
+        
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "goToProfile" {
+//            if let nameToSave = segue.destination as? ProfileViewController {
+//                if let nameToSend = sender as? Entity {
+//                    nameToSave.name = nameToSend}
+//
+//            }
+//    }
+    
+    
     
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
+        
+       
+        
+        
+        
+        
+     
+        
+    
 
+
+
+
+        
 
